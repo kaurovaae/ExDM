a2113ca97f88cd33baf876e084d850b6c06f0cce
kaurovaae <kaurova.a.e@gmail.com>
2023-07-06 11:56:46 +0300

    feat: sidebar

--- src/ui/layout/consts.ts
+++ src/ui/layout/consts.ts
@@ -1,3 +1,23 @@
+import URLS 							from "../../urls";
+
+import {
+	IdcardOutlined,
+	DatabaseOutlined
+} 										from '@ant-design/icons';
+
 export const PREFIX = 'layout';
 
 export const SET_MODEL = 'layout_SET';
+
+export const NAV_ITEMS = [
+	{
+		label: "Справочник",
+		link: URLS.DICTIONARY,
+		icon: IdcardOutlined
+	},
+	{
+		label: "Картотека",
+		link: URLS.PRODUCT,
+		icon: DatabaseOutlined
+	}
+]
--- src/ui/layout/views/index.tsx
+++ src/ui/layout/views/index.tsx
@@ -1,9 +1,12 @@
 import React                    		from "react";
-import {Layout}		 					from "antd";
+import {Layout, Menu}				 	from "antd";
 import {Routes, Route, Navigate}		from "react-router";
 import URLS, {ANY} 						from "../../../urls";
 import Product							from "ui/product";
 import Dictionary						from "ui/dictionary";
+import {NAV_ITEMS} 						from "ui/layout/consts";
+import {Link}							from "react-router-dom";
+
 import dayjs							from "dayjs";
 
 import styles 							from "./index.css";
@@ -16,22 +19,46 @@ dayjs.locale('ru');
 const {Header, Footer, Sider, Content} = Layout;
 
 const LayoutContent: React.FC = (): React.ReactElement => {
+
 	return (
 		<Layout>
+			<Sider
+				breakpoint="lg"
+				collapsedWidth="0"
+				// onBreakpoint={(broken) => {
+				// 	console.log(broken);
+				// }}
+				// onCollapse={(collapsed, type) => {
+				// 	console.log(collapsed, type);
+				// }}
+			>
+				<div className={styles.logo} />
+				<Menu
+					theme="dark"
+					mode="inline"
+					defaultSelectedKeys={['4']}
+					items={NAV_ITEMS.map(
+						(el) => ({
+							key: el.link,
+							icon: React.createElement(el.icon),
+							label: <Link to={el.link}>{el.label}</Link>
+						})
+					)}
+				/>
+			</Sider>
 			<Layout>
-				<Sider className={styles.sider}>SideBar</Sider>
-				<Layout>
-					<Header className={styles.header}>Header</Header>
-					<Content className={styles.content}>
+				<Header className={styles.header} />
+				<Content className={styles.content}>
+					<div className={styles.inner}>
 						<Routes>
 							<Route key={URLS.PRODUCT} path={URLS.PRODUCT + ANY} element={<Product />} />
 							<Route key={URLS.DICTIONARY} path={URLS.DICTIONARY + ANY} element={<Dictionary />} />
 							<Route path='*' element={<Navigate to={URLS.PRODUCT} replace />} />
 						</Routes>
-					</Content>
-				</Layout>
+					</div>
+				</Content>
+				<Footer className={styles.footer}>ExDM © 2023 Created by AnitaChess</Footer>
 			</Layout>
-			<Footer className={styles.footer}>Footer</Footer>
 		</Layout>
 	)
 }
